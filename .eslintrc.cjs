37fad2c6575759c3c458601e09c0096200ab4a78
renan <renanlemos21@gmail.com>
2024-01-19 16:19:05 -0300

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'padded-blocks': ['warn', 'never'],
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'never'],
+    'no-trailing-spaces': ['error', 'never'],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'no-unreachable': ['error'],
+    'no-var': ['error'],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-debugger': ['error'],
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'space-in-parens': ['error', 'never'],
+    'space-before-blocks': ['error', 'always'],
+    'prefer-destructuring': ['error', { object: true, array: false }, { enforceForRenamedProperties: false }],
+    'no-mixed-spaces-and-tabs': ['error'],
+    'require-jsdoc': ['warn'],
+    'arrow-parens': ['error', 'as-needed'],
+    'block-scoped-var': ['error'],
+    'no-else-return': ['error'],
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+    
+    // TypeScript
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/consistent-type-imports': ['off', { prefer: 'type-imports' }],
+    '@typescript-eslint/no-confusing-void-expression': ['error', { ignoreArrowShorthand: true }],
+    '@typescript-eslint/strict-boolean-expressions': 'off'
+  },
+}
