d28af47b11560d6095424f43c372a9495e589c94
renan <renanlemos21@gmail.com>
2024-01-23 13:25:19 -0300

    refactor: update navbarlink style

--- src/components/Navbar/NavbarLink.tsx
+++ src/components/Navbar/NavbarLink.tsx
@@ -1,10 +1,12 @@
 import { TheLink } from '../TheLink/TheLink.tsx'
 import { PropsWithChildren } from 'react'
-import { Box } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 import { LinkProps } from 'react-router-dom'
 
 export const NavbarLink = ({ children, ...rest }: PropsWithChildren<LinkProps>) => {
-  return <Box>
-    <TheLink {...rest}>{children}</TheLink>
-  </Box>
+  return <TheLink {...rest}>
+    <Button variant='ghost'>
+      {children}
+    </Button>
+  </TheLink>
 }
