e9d1ea93e3332467280f8eeb16e3fc8f29b45ba9
renan <renanlemos21@gmail.com>
2024-01-24 09:28:08 -0300

    feat: Create home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,34 @@
+import { Center, Flex, Heading, Text } from '@chakra-ui/react'
+import { CategoryList } from '../components/CategoryItem/CategoryList.tsx'
+import { TheLink } from '../components/TheLink/TheLink.tsx'
 
 export const Home = () => {
-  return <h1>Home</h1>
+  return (
+    <Center mt={20}>
+      <Flex
+        boxShadow='rgba(0, 0, 0, 0.56) 0px 22px 70px 4px'
+        maxW={'880px'}
+        w='60%'
+        borderRadius='8px'
+        p='4rem 5rem'
+        direction='column'
+        gap='2rem'
+      >
+        <Heading w='100%' size='md' colorScheme='red' textAlign='center'>
+          Choose the category you want to discard:
+        </Heading>
+        <Text textDecoration={'underline'} opacity='60%'>
+          <TheLink to={'/collect-places'}>
+            See places closest to your location!
+          </TheLink>
+        </Text>
+        <CategoryList />
+        <Text textDecoration={'underline'} opacity='60%'>
+          <TheLink to={'/faq'}>
+            If you have any questions, visit our FAQ!
+          </TheLink>
+        </Text>
+      </Flex>
+    </Center>
+  )
 }
